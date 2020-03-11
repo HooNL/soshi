97ec79a0984e1e528619450bca7d1433ae3edd09
Hooman Rezaie <48963972+HooNL@users.noreply.github.com>
2020-03-11 20:00:34 +0100

    This is another one

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,9 +1,11 @@
 <template>
-  <section class="about">
+  <article class="about">
     <h1>This is an about page</h1>
 
 
-
+    <section>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat dolorum quae veniam earum assumenda aliquam vero suscipit itaque quibusdam dolorem, reprehenderit soluta nobis repellendus deserunt sit consectetur enim quaerat! Eius maiores, laborum consequuntur aperiam nam ipsum quae at rem id mollitia blanditiis esse praesentium repellendus accusantium voluptate in molestiae sequi error eum similique libero soluta eos magnam! Nemo sunt quo illo cupiditate facilis itaque, quisquam quibusdam modi assumenda perspiciatis natus! Quaerat unde reiciendis fugiat officiis. Aspernatur error, consequuntur numquam totam, libero doloremque enim veritatis voluptates aliquam corrupti earum unde quaerat quam debitis sunt ullam accusamus. Doloribus earum at veritatis iusto minima. Vero dicta magni debitis quisquam cum, deserunt molestiae dolorem perferendis tenetur accusamus unde necessitatibus vitae soluta cupiditate dignissimos consequatur placeat ad repudiandae aut, beatae deleniti fugit voluptates eius! Optio sint soluta repellendus quaerat quam itaque numquam, debitis placeat amet, incidunt odit nemo quos illo laudantium enim mollitia sed odio aliquam aliquid unde consectetur rem asperiores? Pariatur voluptatibus maxime sit dolore odio animi nisi consequuntur vero, obcaecati labore illum maiores ad ipsam ratione eligendi, commodi beatae ullam eius distinctio? Dolorem nam officia iusto eaque labore explicabo ratione veniam eos accusamus mollitia suscipit odio consequuntur laudantium odit soluta, quibusdam cupiditate aliquid!
+    </section>
     
-  </section>
+  </article>
 </template>
